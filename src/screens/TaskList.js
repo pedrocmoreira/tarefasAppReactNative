9c975a22ebacbac798549bdd3c874b03941dd4f1
Pedro Moreira <pedrocmoreira97@gmail.com>
2020-01-30 21:59:33 -0300

    Adicionando funcionalidades de check nas tarefas

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {View, Text, ImageBackground, StyleSheet} from "react-native";
+import {View, Text, ImageBackground, StyleSheet, FlatList} from "react-native";
 
 import commonStyles from "../commonStyles";
 import todayImage from "../../assets/imgs/today.jpg";
@@ -10,6 +10,31 @@ import "moment/locale/pt-br";
 import Task from "../components/Task"
 
 export default class TaskList extends Component{
+    state = {
+        tasks: [{
+            id: Math.random(),
+            desc: "Comprar Livro de React Native",
+            estimateAt: new Date(), 
+            doneAt: new Date(), 
+        },{
+            id: Math.random(),
+            desc: "Ler Livro de React Native",
+            estimateAt: new Date(), 
+            doneAt: null,
+        }]
+    }
+
+    toggleTask = taskId => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if(task.id === taskId) {
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+        });
+
+        this.setState({tasks});
+    }
+
     render(){
         const today = moment().locale("pt-br").format("ddd, D [de] MMMM");
         return(
@@ -21,10 +46,8 @@ export default class TaskList extends Component{
                     </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <Task desc="Comprar livro" estimateAt={new Date()} doneAt={new Date()}/>
-                    <Task desc="Ler livro" estimateAt={new Date()} doneAt={null}/>
+                    <FlatList data={this.state.tasks} keyExtractor={item =>`${item.id}`} renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask}/>}/>
                 </View>
-                
             </View>
         )
     }
