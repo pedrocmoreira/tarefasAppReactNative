4172682011f34192bd3a91cf64f609132ffd1897
Pedro Moreira <pedrocmoreira97@gmail.com>
2020-02-02 21:25:31 -0300

    Adicionado o botão de Nova Tarefa, para que um modal seja aberto com o input de nova tarefa

---
+++ src/screens/AddTask.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { Modal, View, StyleSheet, TouchableWithoutFeedback,TouchableOpacity, TextInput, Text } from "react-native";
+
+import commonStyles from "../commonStyles";
+
+const initialState ={ desc: ""}
+
+export default class AddTask extends Component {
+    state = {
+        ...initialState
+    }
+
+    render() {
+        return (
+            <Modal transparent={true} visible={this.props.isVisible} onRequestClose={this.props.onCancel}
+                animationType="slide">
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header} >Nova Tarefa</Text>
+                    <TextInput style={styles.input} placeholder="Infrome a Descrição..."
+                    onChangeText={desc => this.setState({desc})} value={this.state.desc}/>
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+            </Modal>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1, 
+        backgroundColor: "rgba(0, 0, 0, 0.7)"
+    },
+    container: {
+       // flex: 1,
+        backgroundColor: "#FFF", 
+
+    }, 
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: "center",
+        padding: 15,
+        fontSize: 18,
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        //width: "90%",
+        height: 40,
+        margin: 15,
+        marginLeft: 10,
+        backgroundColor:"#FFF",
+        borderWidth: 1,
+        borderColor: "#E3E3E3",
+        borderRadius: 6,
+    },
+    buttons: {
+        flexDirection:"row",
+        justifyContent: "flex-end"
+    },
+    button: {
+        margin: 20, 
+        marginRight: 30,
+        color: commonStyles.colors.today,
+
+    }
+})
+ 
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -10,10 +10,12 @@ import "moment/locale/pt-br";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 import Task from "../components/Task"
+import AddTask from "./AddTask";
 
 export default class TaskList extends Component{
     state = {
         showDoneTasks: true,
+        showAddTask: false, 
         visibleTasks: [],
         tasks: [{
             id: Math.random(),
@@ -63,6 +65,8 @@ export default class TaskList extends Component{
         const today = moment().locale("pt-br").format("ddd, D [de] MMMM");
         return(
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask} 
+                    onCancel={() => this.setState({showAddTask: false})}/>
                 <ImageBackground source={todayImage} style={styles.background}>
                     <View style={styles.iconBar}>
                         <TouchableOpacity onPress={this.toggleFilter}>
@@ -78,11 +82,16 @@ export default class TaskList extends Component{
                 <View style={styles.taskList}>
                     <FlatList data={this.state.visibleTasks} keyExtractor={item =>`${item.id}`} renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask}/>}/>
                 </View>
+                <TouchableOpacity style={styles.addButton}
+                    activeOpacity={0.7} onPress={() => this.setState({ showAddTask:true })}>
+                    <Icon name="plus" size={20} color={commonStyles.colors.secondary} />
+                </TouchableOpacity>
             </View>
         )
     }
 }
 
+//Estilos 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -116,6 +125,17 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         justifyContent: "flex-end",
         marginTop: Platform.OS === "ios" ? 40 : 10,
+    },
+    addButton:{
+        position: "absolute",
+        right: 30,
+        bottom: 30,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: commonStyles.colors.today,
+        justifyContent: "center",
+        alignItems: "center",
     }
 })
 
